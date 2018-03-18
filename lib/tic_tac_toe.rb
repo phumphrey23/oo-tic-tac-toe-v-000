4217246ae61534948ec91a318bfa102fb0ba17de
Precious Humphrey <phumphrey23@gmail.com>
2018-03-18 14:48:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,10 +65,9 @@ class TicTacToe
          return win_combination
        elsif @board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O"
          return win_combination
-       else
-         false
        end
-     end
+         false
+      end
    end
 
    def full?
