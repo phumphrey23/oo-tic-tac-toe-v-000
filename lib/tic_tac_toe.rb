e85f33bc87a38326b42731eafaac37af8ec67fda
Precious Humphrey <phumphrey23@gmail.com>
2018-03-18 15:06:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,7 @@ class TicTacToe
    display_board
   end
 
-  def won?(board)
+  def won?
   WIN_COMBINATIONS.detect do |win_combination|
        win_index_1 = win_combination[0]
        win_index_2 = win_combination[1]
